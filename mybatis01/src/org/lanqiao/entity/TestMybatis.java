cadd78c75e07712d051d64447e9b11c47ed2dd6b
xt <xt_cloud.com>
2020-10-12 15:09:16 +0800

    发布项目

---
+++ mybatis01/src/org/lanqiao/entity/TestMybatis.java
@@ -0,0 +1,26 @@
+package org.lanqiao.entity;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class TestMybatis {
+	public static void main(String[] args) throws IOException {
+		//加载MyBatis配置文件（为了访问数据库）
+		Reader reader = Resources.getResourceAsReader("conf.xml") ;
+		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader) ;
+		//session - connection
+		
+		
+		SqlSession session = sessionFactory.openSession() ;
+		String statement = "org.lanqiao.entity.personMapper.queryPersonById" ;
+		Person person = session.selectOne( statement,1 ) ;
+		System.out.println(person);
+		session.close(); 
+		
+	}
+}
